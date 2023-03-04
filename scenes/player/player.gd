5289757b8c2ca4c19c593926ad110772017a7dcd
ColeWarden <cwarden2002@gmail.com>
2023-03-04 01:33:56 -0600

    added sprite, animation and some tiles
    added player animation with eddy and rexy, hurt and hit area templates added in

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
+const DEAD_ZONE_LIMIT = 0.5
+const MAX_HORZ_VELOCITY = 2.0
+const MAX_JUMP_VELOCITY = 8.0
 
-const MAX_HORZ_VELOCITY = 5.0
-const MAX_JUMP_VELOCITY = 9.0
-
-const HORZ_VELOCITY = 5.0
+const HORZ_VELOCITY = 1.0
 const JUMP_VELOCITY = 9.0
 
 var controller_id: int = -1 setget set_controller_id
@@ -12,10 +12,13 @@ var velocity: Vector2 = Vector2.ZERO
 var gravity: float = 0.5
 var friction: float = 300.0
 
+onready var sprite: Sprite = $Sprite
 onready var collisionShape2D: CollisionShape2D = $CollisionShape2D
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	set_process(false)
+	animationPlayer.playback_speed = 10.0
 
 
 func set_controller_id(value: int)-> void:
@@ -24,46 +27,28 @@ func set_controller_id(value: int)-> void:
 
 
 func _process(delta: float) -> void:
-	var horz_dir: float = 0.0
-	
 	# Horz Movement
-	horz_dir = sign(Input.get_joy_axis(controller_id, JOY_ANALOG_LX))
+	var horz_dir: float = Input.get_joy_axis(controller_id, JOY_ANALOG_LX)
+	if abs(horz_dir) < DEAD_ZONE_LIMIT:
+		horz_dir = 0.0
 	if horz_dir != 0.0:
+		if animationPlayer.current_animation != "jump":
+			animationPlayer.play("walk")
+		sprite.flip_h = horz_dir > 0.0
 		velocity.x += HORZ_VELOCITY * horz_dir
 	else:
+		if animationPlayer.current_animation != "jump":
+			animationPlayer.play("idle")
 		velocity.x = move_toward(velocity.x, 0.0, friction * delta)
 	
 	# Vert Movement
 	# If jump is ready and B pressed 
-	velocity.y += gravity
-#	if $RayCast2D.is_colliding():
-#		velocity.y = 0.0
-#	var collision_pos: Vector2 = collisionShape2D.global_position
-#	var rectShape: RectangleShape2D = collisionShape2D.shape
-#	var extents: Vector2 = rectShape.extents
-#	var bottom_left_pos: Vector2 = collision_pos + Vector2(-extents.x, extents.y)
-#	var bottom_right_pos: Vector2 = collision_pos + Vector2(extents.x, extents.y)
-#	var slides: int = get_slide_count()
-#	var on_ground: bool = slides != 0
-#	for slide in slides:
-#		var collision: KinematicCollision2D = get_slide_collision(slide)
-#		var pos: Vector2 = collision.position + Vector2(8,8)
-#		var on_ground_tile: bool = false
-#		if pos.x < collision_pos.x:
-#			on_ground_tile = abs(bottom_left_pos.direction_to(pos).x) <= PI/4
-#		else:
-#			on_ground_tile = abs(bottom_right_pos.direction_to(pos).x) <= PI/4
-#		if !on_ground_tile:
-#			on_ground = false
-#			break
-#
-#	if is_on_floor():
-#		velocity.y = 0.0
-	#print(velocity)
-	
 	if Input.is_joy_button_pressed(controller_id, JOY_XBOX_B) and is_on_floor():
+		animationPlayer.play("jump")
 		velocity.y = -JUMP_VELOCITY
-		#velocity.y -= JUMP_VELOCITY
+	
+	if !is_on_floor():
+		velocity.y += gravity
 	
 	var clamped_x: float = clamp(velocity.x, -MAX_HORZ_VELOCITY, MAX_HORZ_VELOCITY)
 	var clamped_y: float = clamp(velocity.y, -MAX_JUMP_VELOCITY, MAX_JUMP_VELOCITY)
